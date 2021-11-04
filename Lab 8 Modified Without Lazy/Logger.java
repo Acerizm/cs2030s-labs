9d0bc8cae4d42d070d9f3ee8fa603e8dee3ba9c3
Haiqel <acehql3@gmail.com>
2021-11-04 13:12:25 +0800

    Level 2 complete

--- Lab 8 Modified Without Lazy/Logger.java
+++ Lab 8 Modified Without Lazy/Logger.java
@@ -6,13 +6,13 @@ class Logger<T> {
     private final String outputOfValues;
 
     //constructor
-    //private means that i STILL CAN USE IT within this class
+    //private means that I STILL CAN USE IT within this class
     private Logger(T value) {
         this.value = value;
-        outputOfValues = "" + value;
+        outputOfValues = "";
     }
 
-    private Logger( T value, String values) {
+    private Logger(T value, String values) {
         this.value = value;
         outputOfValues = values;
     }
@@ -24,8 +24,7 @@ class Logger<T> {
         }
         Optional<T> optional = Optional.ofNullable(value);
         optional.ifPresentOrElse(
-            x -> {} // do nothing
-            ,
+            x -> { }, // do nothing      
             () -> {
                 throw new IllegalArgumentException("argument cannot be null");
             }
@@ -48,15 +47,25 @@ class Logger<T> {
         //     return new Logger<U>(mapper.apply(this.value));
         // }
         //fix this algo here
+        // I printed to a string because the string "can store any type".
+        // You can also choose to store the outputs into a list but wgt
         T oldValue = this.value;
         U newValue = mapper.apply((this.value));
-        String newOutputValues = this.outputOfValues + "->" + newValue + "\n";
+        String newOutputValues = this.outputOfValues + "\n" + oldValue + " -> " + newValue;
+        //I am using the private constructor NOT the static factory method to 
+        //construct the Logger :D
         return new Logger<U>(newValue,newOutputValues);
     }
 
     //toString method
     @Override
     public String toString() {
-        return "Logger[" + this.value + "]\n" + this.outputOfValues;
+        if (this.outputOfValues == "") {
+            // Codecrunch not happy if there is a newLine when there 
+            // is no output of values xd
+            return "Logger[" + this.value + "]";
+        } else {
+            return "Logger[" + this.value + "]" + this.outputOfValues;
+        }
     }
 }
